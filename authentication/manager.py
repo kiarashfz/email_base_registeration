d6c1d14218d3b375899617595b1122ecf5d3fe49
kiarashfz <kiawfz3673@gmail.com>
2022-09-09 16:12:17 +0430

    CustomUserManager inherited from BaseUserManager instead of UserManager & username field completely removed from it

--- authentication/manager.py
+++ authentication/manager.py
@@ -1,16 +1,35 @@
-from django.contrib.auth.models import UserManager
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import ugettext_lazy as _
 
 
-class CustomUserManager(UserManager):
-    # Username is a required field in protected method _create_user in UserManager
-    # I want email base registration without username
-    # I didn't override _create_user method because it's protected
-    # Now username is an optional field that filled by email
-    def create_superuser(self, username=None, email=None, password=None, **extra_fields):
-        username = email
-        return super().create_superuser(username, email, password, **extra_fields)
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
 
-    def create_user(self, username=None, email=None, password=None, **extra_fields):
-        username = email
-        return super().create_user(username, email, password, **extra_fields)
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
 
