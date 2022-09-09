bee7d921fa576fc3dc4ccf5da8b27984f02a15f0
kiarashfz <kiawfz3673@gmail.com>
2022-09-10 00:01:49 +0430

    swagger url patterns moved to main urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,14 +2,10 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from authentication.apis import UserViewSet
-from django.conf.urls import url
-
-from authentication.views import schema_view
 
 router = DefaultRouter()
 router.register('', UserViewSet)
 
 urlpatterns = [
     path('users/', include(router.urls)),
-    url(r'^$', schema_view)
 ]
