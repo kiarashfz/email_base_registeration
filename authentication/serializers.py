2736d18664625f88eb48f3461aeb451504c01e98
kiarashfz <kiawfz3673@gmail.com>
2022-09-09 22:10:14 +0430

    password add to serializer as a required field & hashed to save in DB

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 
 from authentication.models import User
@@ -6,4 +7,9 @@ from authentication.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email']
+        fields = ['id', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super(UserSerializer, self).create(validated_data)
