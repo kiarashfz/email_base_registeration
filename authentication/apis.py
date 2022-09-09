7075ba8d90f68e63f709c07cee847a80283213d7
kiarashfz <kiawfz3673@gmail.com>
2022-09-09 22:27:41 +0430

    document text change to a comment

--- authentication/apis.py
+++ authentication/apis.py
@@ -5,8 +5,6 @@ from authentication.serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    User ModelViewSet used to have all HTTP methods(GET, POST, PUT, PATCH, DELETE) for User objects as api
-    """
+    # User ModelViewSet used to have all HTTP methods(GET, POST, PUT, PATCH, DELETE) for User objects as api
     queryset = User.objects.all()
     serializer_class = UserSerializer
