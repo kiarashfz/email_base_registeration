c15aaeb7aab3eeff4974cd80049b3c647edb00f6
kiarashfz <kiawfz3673@gmail.com>
2022-09-09 15:02:29 +0430

    Custom User model (email base) created using CustomUserManager

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from authentication.manager import CustomUserManager
+
+
+class User(AbstractUser):
+    objects = CustomUserManager()
+
+    # Email as a username field must be unique
+    # I've just added a unique constraint to email field instead of defining again email field
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['email'], name='email')
+        ]
+
+    # The field named as the 'USERNAME_FIELD' for a custom user model must not be included in 'REQUIRED_FIELDS'
+    REQUIRED_FIELDS = AbstractUser.REQUIRED_FIELDS[:]
+    # Removing email from REQUIRED_FIELDS without changing parent REQUIRED_FIELDS class attribute
+    REQUIRED_FIELDS.remove('email')
+    USERNAME_FIELD = 'email'
