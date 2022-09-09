a86830135cb54af86a4252bac9e78920b15b92a7
kiarashfz <kiawfz3673@gmail.com>
2022-09-09 18:52:31 +0430

    swagger schema view added to urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,10 +2,14 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from authentication.apis import UserViewSet
+from django.conf.urls import url
+
+from authentication.views import schema_view
 
 router = DefaultRouter()
 router.register('', UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('users/', include(router.urls)),
+    url(r'^$', schema_view)
 ]
