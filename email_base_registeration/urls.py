1ed01965f6a9c7a1313f36dbcbd8bc7c00422940
kiarashfz <kiawfz3673@gmail.com>
2022-09-10 14:32:31 +0430

    STATIC_ROOT added to urlpatterns for deploy time

--- email_base_registeration/urls.py
+++ email_base_registeration/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 
 from email_base_registeration.swagger_schema import schema_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authentication.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # swagger urls
 urlpatterns += [
