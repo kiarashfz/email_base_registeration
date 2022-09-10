6b0ba0eac950c75bdfad3c1816bab74230156503
kiarashfz <kiawfz3673@gmail.com>
2022-09-10 21:16:50 +0430

    ready method wrote in apps file to load signals

--- authentication/apps.py
+++ authentication/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'authentication'
+
+    def ready(self):
+        from authentication import signals
