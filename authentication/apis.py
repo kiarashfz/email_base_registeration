127ac9ddb4609217bf89e2c4fa5de056c3121ba0
kiarashfz <kiawfz3673@gmail.com>
2022-09-10 15:57:01 +0430

    Users apis uses my custom UserPermission and the BasicAuthentication class set for apis for now

--- authentication/apis.py
+++ authentication/apis.py
@@ -1,10 +1,16 @@
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAdminUser
 
 from authentication.models import User
+from authentication.permissions import UserPermission
 from authentication.serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     # User ModelViewSet used to have all HTTP methods(GET, POST, PUT, PATCH, DELETE) for User objects as api
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [UserPermission]
     queryset = User.objects.all()
     serializer_class = UserSerializer
