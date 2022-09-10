9de375a3bae4e0a5f8c78bd6bd323e67a878d5c0
kiarashfz <kiawfz3673@gmail.com>
2022-09-10 22:06:10 +0430

    runner bash script wrote to automated deployment

---
+++ runner.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Author: KiarashFz
+# Date Created: 10 sep 2022
+# Date Modified: 10 sep 2022
+# Description: wrote for automated deployment
+
+sudo docker-compose up --build -d
+
+exit 0
