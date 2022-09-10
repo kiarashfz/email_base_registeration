42a2e2b82d06fd7ee06053145573870b724bbff5
kiarashfz <kiawfz3673@gmail.com>
2022-09-10 18:07:56 +0430

    swagger doc todo added

--- authentication/apis.py
+++ authentication/apis.py
@@ -1,7 +1,6 @@
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAdminUser
 
 from authentication.models import User
 from authentication.permissions import UserPermission
@@ -10,6 +9,7 @@ from authentication.serializers import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     # User ModelViewSet used to have all HTTP methods(GET, POST, PUT, PATCH, DELETE) for User objects as api
+    # todo: swagger doc
     authentication_classes = [BasicAuthentication]
     permission_classes = [UserPermission]
     queryset = User.objects.all()
