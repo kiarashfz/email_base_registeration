03829ddb6ad295e76fe289d3ac4935d95c9c34b6
kiarashfz <kiawfz3673@gmail.com>
2022-09-10 14:31:52 +0430

    STATIC_ROOT added for deploy time

--- email_base_registeration/settings.py
+++ email_base_registeration/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-q_la3s@38lt8oyrcai*j_dq7u6ve3cy567p_f7^_wqpv-%i^#=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
