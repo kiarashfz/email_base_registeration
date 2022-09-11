cd39741edc4c158f78c7b84c9e846ed6c28a861e
kiarashfz <kiawfz3673@gmail.com>
2022-09-11 19:32:23 +0430

    added api path for users apis

--- email_base_registeration/urls.py
+++ email_base_registeration/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('authentication.urls')),
+    path('api/', include('authentication.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # swagger urls
