a29b3649acd6e3fec678e189535a46b70ca04cd6
kiarashfz <kiawfz3673@gmail.com>
2022-09-11 23:49:46 +0430

    operation_description added to all apis

--- authentication/apis.py
+++ authentication/apis.py
@@ -9,8 +9,30 @@ from authentication.serializers import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     # User ModelViewSet used to have all HTTP methods(GET, POST, PUT, PATCH, DELETE) for User objects as api
-    # todo: swagger doc
     authentication_classes = [BasicAuthentication]
     permission_classes = [UserPermission]
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @swagger_auto_schema(operation_description="Superuser have access to users list.")
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @swagger_auto_schema(operation_description="Each user can see own object. Superuser have access to see all users.")
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Each user can delete own object. Superuser have access to delete all users.")
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Each user can update own object. Superuser have access to update all users.")
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Each user can update own object. Superuser have access to update all users.")
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
