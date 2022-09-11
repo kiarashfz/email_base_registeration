2a8bf7fb9135ec4eb29de18e5a0207245042f847
kiarashfz <kiawfz3673@gmail.com>
2022-09-11 23:27:20 +0430

    get my user test wrote & basic auth function created

--- authentication/tests_apis.py
+++ authentication/tests_apis.py
@@ -13,10 +13,20 @@ class AccountTests(APITestCase, URLPatternsTestCase):
     admin_email = 'admin@admin.admin'
     admin_password = 'admin'
 
+    user_email = 'user@user.user'
+    user_password = 'user'
+
     @classmethod
     def setUpTestData(cls):
-        User.objects.create_superuser(cls.admin_email, cls.admin_password)
+        cls.admin = User.objects.create_superuser(cls.admin_email, cls.admin_password)
+        cls.user = User.objects.create_user(cls.user_email, cls.user_password)
         super().setUpTestData()
+
+    def set_basic_authentication(self, is_admin: bool) -> User:
+        username_password = f'{self.admin_email}:{self.admin_password}'.encode() if is_admin else f'{self.user_email}:{self.user_password}'.encode()
+        encoded_data = str(base64.b64encode(username_password).decode())
+        self.client.credentials(HTTP_AUTHORIZATION=f"Basic {encoded_data}")
+        return self.admin if is_admin else self.user
 
     def test_create_user(self):
         """
@@ -30,18 +40,23 @@ class AccountTests(APITestCase, URLPatternsTestCase):
         response = self.client.post(url, data=data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['email'], data['email'])
-        self.assertEqual(response.data['id'], 2)  # Expected id is 2 because id 1 is superuser
 
     def test_get_all_users(self):
         """
         Ensure superuser can get all user objects.
         """
         url = reverse('user-list')
-
-        # Using Basic authentication
-        username_password = f'{self.admin_email}:{self.admin_password}'.encode()
-        encoded_data = str(base64.b64encode(username_password).decode())
-        self.client.credentials(HTTP_AUTHORIZATION=f"Basic {encoded_data}")
-
+        self.set_basic_authentication(is_admin=True)
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_my_user(self):
+        """
+        Ensure user can get own user object.
+        """
+        url = reverse('user-list')
+        user = self.set_basic_authentication(is_admin=False)
+        user_url = f'{url}{user.id}/'
+        response = self.client.get(user_url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['id'], user.id)
